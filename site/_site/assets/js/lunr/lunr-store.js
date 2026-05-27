cc3de6f98337db2cb0f4847f4d91b7af0af904b5
Nam Dang <v.namdp15@vinmotion.net>
2026-05-27 18:23:57 +0700

    fix: split deploy into separate job

--- site/_site/assets/js/lunr/lunr-store.js
+++ site/_site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Daily Digest 2026-05-27",
-        "excerpt":"The dominant theme across today's content centers on advancing autonomous systems and their real-world deployment, with a focus on long-term agent interactions, uncertainty management, and improving the reliability and scalability of AI-driven processes. Global Trends Papers discovered from ArXiv subject categories AI Safety 4/5 Artificial Intelligence (cs.AI) 27 May 2026...","categories": [],
+        "excerpt":"The dominant theme across today's content centers on advancing autonomous, adaptive, and human-aligned AI systems, with a focus on long-term interaction capabilities, ethical alignment, and real-world applicability. Global Trends Papers discovered from ArXiv subject categories AI Safety 4/5 Artificial Intelligence (cs.AI) 27 May 2026 OmniToM: Benchmarking Theory of Mind in...","categories": [],
         "tags": [],
         "url": "/ai-digest-daily/digest/2026-05-27/",
         "teaser": null
